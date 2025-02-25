ce981ea496f041aaabb64d50e9cada35d14e64b6
saim-alam1 <allbehassansaim@gmail.com>
2025-02-25 15:17:15 +0600

    Module 27.3 video

--- scripts/login.js
+++ scripts/login.js
@@ -1,6 +1,26 @@
 // console.log('The Batman');
 
 document.getElementById('login-btn')
-  .addEventListener('click', function () {
-    console.log('Btn clicked');
+  .addEventListener('click', function (event) {
+    event.preventDefault();
+
+    const accountNumber = document.getElementById('account-number').value;
+
+    const pinNumber = document.getElementById('pin-number').value;
+    const convertedPin = parseInt(pinNumber);
+
+    if (accountNumber.length === 11) {
+
+      if (convertedPin === 1234) {
+        window.location.href = './main.html'
+      }
+      else {
+        alert('pin thik nai');
+      }
+
+    }
+    else {
+      alert('need valid number');
+    }
+
   })
